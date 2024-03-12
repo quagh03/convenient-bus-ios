3a9a5d43ccab77ca30fd999ee115c36ad4693b10
HuuHieuu <babymilk1904@gmail.com>
2024-03-12 11:52:05 +0700

    Initial Commit

---
+++ Bus/BusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusApp.swift
+//  Bus
+//
+//  Created by Nguyễn Hữu Hiếu on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
