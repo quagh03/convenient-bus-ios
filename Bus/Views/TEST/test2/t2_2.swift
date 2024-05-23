c49597a338b9bc27808e4756ad6bc6d8cdeadfb9
HuuHieuu <babymilk1904@gmail.com>
2024-05-23 11:10:07 +0700

    Complete

---
+++ Bus/Views/TEST/test2/t2_2.swift
@@ -0,0 +1,18 @@
+//
+//  t2_2.swift
+//  Bus
+//
+//  Created by Nguyễn Hữu Hiếu on 22/5/24.
+//
+
+import SwiftUI
+
+struct t2_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    t2_2()
+}
