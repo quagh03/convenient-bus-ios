7a31af08ba02eb098466d885d0cbfc4b51c9e295
HuuHieuu <babymilk1904@gmail.com>
2024-05-31 10:36:03 +0700

    fix bug: valid information

---
+++ Bus/Reuseable/ToastM1.swift
@@ -0,0 +1,18 @@
+//
+//  ToastM1.swift
+//  Bus
+//
+//  Created by Nguyễn Hữu Hiếu on 27/5/24.
+//
+
+import SwiftUI
+
+struct ToastM1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToastM1()
+}
